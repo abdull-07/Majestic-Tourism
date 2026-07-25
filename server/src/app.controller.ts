560b489a8229c89bed4c4d3ce7faa3f632c4806b
abdull-07 <personal.muhammadabdullah@gmail.com>
2026-07-25 18:26:02 +0500

    add prisma module and service

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { PrismaService } from './prisma/prisma.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private prisma: PrismaService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('health')
+  async health() {
+    const userCount = await this.prisma.user.count();
+    return { status: 'ok', userCount };
   }
 }
