7b83be523c494e4377a76522991e9df0f625af19
abdull-07 <personal.muhammadabdullah@gmail.com>
2026-09-16 15:46:01 +0500

    Revert "Add Tour Page and its components"
    This reverts commit 8a7182ffbb06165799bdee1284cd9dad8340c5ae.

--- client/src/lib/api/tours.ts
+++
@@ -1,8 +0,0 @@
-import api from '../api';
-import type { Tour, TourDetail } from '../../types/tour';
-
-export const toursApi = {
-    getAllActive: (filters: { destinationId?: string; category?: string } = {}) =>
-        api.get<Tour[]>('/tours', { params: filters }).then((r) => r.data),
-    getBySlug: (slug: string) => api.get<TourDetail>(`/tours/${slug}`).then((r) => r.data),
-};
